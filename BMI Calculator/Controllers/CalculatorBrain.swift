fa00c0aea3f556627067833830384e149aaa7ed2
LindaEl <>
2021-08-29 18:35:15 +0300

    Adding CalculatorBrain for MVC Design

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,23 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by linda.elksnite on 29/08/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+    var bmi: Float?
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        bmi = weight / pow(height, 2)
+    }
+    
+    func getBMIValue() -> String {
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi ?? 0.0)
+        return bmiTo1DecimalPlace
+    }
+}
---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,9 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by linda.elksnite on 29/08/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
